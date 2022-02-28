c2b4d4a6f44940ff5b3e5b6250aa328c28119e4d
DH Lee <78197111+dhleeone@users.noreply.github.com>
2022-02-28 15:06:43 +0900

    level2_타겟넘버[미완료], 파일명정렬[미완료]

---
+++ programmers/level2_타겟넘버[미완료].py
@@ -0,0 +1,21 @@
+def solution(files):
+    li=[]
+    for file in files:
+        for i in range(len(file)):
+            if file[i].isdecimal() == True:
+                head = file[:i]
+                first_num = i
+                break
+        for j in range(first_num, len(file)):
+            if file[j].isdecimal() == False:
+                num = file[first_num:j]
+                tail = file[j:]
+                break
+        li.append([head, num, tail])
+        li.sort(key=lambda x: int(x[1]))
+        li.sort(key=lambda x: x[0].lower())
+
+
+        answer = ["".join(i) for i in li]
+
+    return answer
