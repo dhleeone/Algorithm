79980bc01b30f4d5a220c9aa4b255d194c7578fd
DH Lee <78197111+dhleeone@users.noreply.github.com>
2022-03-10 19:38:50 +0900

    level2_수식최대화

---
+++ programmers/level2_수식최대화.py
@@ -0,0 +1,49 @@
+from itertools import permutations
+
+def solution(expression):
+    ops = ['+', '-', '*']
+    ops = list(permutations(ops))
+    
+    exp = []
+    num = ""
+    for i in range(len(expression)):
+        if expression[i].isdigit():
+            num += expression[i]
+
+        elif not expression[i].isdigit():
+            exp.append(int(num))
+            exp.append(expression[i])
+            num = ""
+        if i == len(expression) - 1:
+            exp.append(int(num))
+            
+    answer_li = []
+    for op in ops:
+        exp_c = exp.copy()
+        for idx in range(3):
+            cnt = exp_c.count(op[idx])
+            for _ in range(cnt):
+                for i in range(1, len(exp_c)-1):
+                    if exp_c[i] == op[idx]:
+                        if op[idx] == "-":
+                            minus = exp_c[i-1] - exp_c[i+1]
+                            exp_c[i-1] = minus
+                            del exp_c[i:i+2]
+                            break
+
+                        elif op[idx] == "+":
+                            plus = exp_c[i-1] + exp_c[i+1]
+                            exp_c[i-1] = plus
+                            del exp_c[i:i+2]
+                            break
+
+                        elif op[idx] == "*":
+                            mul = exp_c[i-1] * exp_c[i+1]
+                            exp_c[i-1] = mul
+                            del exp_c[i:i+2]
+                            break
+
+        answer_li.append(abs(exp_c[0]))
+    answer = max(answer_li)
+    
+    return answer
