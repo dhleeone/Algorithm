d99521e105f04dc68431f30e87d3be7793e6e674
DH Lee <78197111+dhleeone@users.noreply.github.com>
2022-04-21 10:11:31 +0900

    level2_뉴스클러스터링

--- programmers/level2_뉴스클러스터링.py
+++ programmers/level2_뉴스클러스터링.py
@@ -7,17 +7,14 @@ def preprocess(string):
     return li
 
 def unionprocess(string, inner):
-    li = []
-    for s in string:
-        if s not in inner:
-            li.append(s)
+    li = [s for s in string if s not in inner]
     return li
 
 def solution(str1, str2):
     str_1, str_2 = preprocess(str1), preprocess(str2)
     inner = list(set(str_1)&set(str_2))
     outer = len(unionprocess(str_1, inner)+unionprocess(str_2, inner))
-    min_num, max_num = 0
+    min_num, max_num = 0, 0
     
     for i in inner:
         a = str_1.count(i)
@@ -29,4 +26,3 @@ def solution(str1, str2):
         return 65536
     answer = int((min_num / outer) * 65536)
     return answer
-    
