1252c12249fdc4597b93e5af6b300af7f2c691a4
DH Lee <78197111+dhleeone@users.noreply.github.com>
2022-04-21 22:00:26 +0900

    level3_가장먼노드

--- programmers/level3_가장먼노드.py
+++ programmers/level3_가장먼노드.py
@@ -1,3 +1,4 @@
+# 풀이 1
 from collections import deque
 
 def solution(n, edge):
@@ -18,5 +19,26 @@ def solution(n, edge):
                 cnt_li[nnd] += (cnt_li[node] + 1)
     return cnt_li.count(max(cnt_li))
 
-
 # 8, 9번 시간초과
+
+# 풀이 2
+from collections import deque
+
+def solution(n, edge):
+    graph = [[] for _ in range(n+1)]
+    for a, b in edge:
+        graph[a].append(b)
+        graph[b].append(a)
+        
+    cnt_li = [0 for _ in range(n+1)]           
+    queue = deque([1])
+    while queue:
+        node = queue.popleft()
+        for nnd in graph[node]:
+            if cnt_li[nnd] == 0:
+                queue.append(nnd)
+                cnt_li[nnd] += (cnt_li[node] + 1)
+    cnt_li[1] = 0
+    return cnt_li.count(max(cnt_li))
+
+# 방문 리스트 없이 cnt로 방문 구분
