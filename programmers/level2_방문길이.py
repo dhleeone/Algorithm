f060bf551b7608c02ecf2855f29bf60a10c76297
DH Lee <78197111+dhleeone@users.noreply.github.com>
2022-04-26 12:26:33 +0900

    level2_방문길이

--- programmers/level2_방문길이.py
+++ programmers/level2_방문길이.py
@@ -3,31 +3,26 @@ def solution(dirs):
     set_li = []
     
     for di in dirs:
-        x = li[-1][0]
-        y = li[-1][1]
+        x, y = li[-1][0], li[-1][1]
         if di == "U" and y < 5:
-            nx = x
-            ny = y+1
+            nx, ny = x, y+1
             li.append((nx, ny))
             set_li.append((x+nx, y+ny))
             
         elif di == "D" and y > -5:
-            nx = x
-            ny = y-1
+            nx, ny = x, y-1
             li.append((nx, ny))
             set_li.append((x+nx, y+ny))
             
         elif di == "L" and x > -5:
-            nx = x-1
-            ny = y
+            nx, ny = x-1, y
             li.append((nx, ny))
             set_li.append((x+nx, y+ny))
         
         elif di == "R" and x < 5:
-            nx = x+1
-            ny = y
+            nx, ny = x+1, y
             li.append((nx, ny))     
             set_li.append((x+nx, y+ny))
-
+            
     answer = len(set(set_li))
     return answer
