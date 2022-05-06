0c2f4019bfe369ee975388569b6ee8f773aa4294
DH Lee <78197111+dhleeone@users.noreply.github.com>
2022-05-06 11:15:28 +0900

    level2_거리두기확인하기

--- programmers/level2_거리두기확인하기.py
+++ programmers/level2_거리두기확인하기.py
@@ -1,27 +1,21 @@
 from collections import deque
 
-def matrix(place):
-    mat = []
-    for row in place:
-        mat.append(list(row))
-    return mat
-
-def loc_p(mat):
+def loc_p(place):
     p_li = []
-    for i in range(len(mat)):
-        for j in range(len(mat[0])):
-            if mat[i][j] == "P":
+    for i in range(len(place)):
+        for j in range(len(place[0])):
+            if place[i][j] == "P":
                 p_li.append((i, j))
     return p_li
 
-def bfs(mat, a, b):
+def bfs(place, a, b):
     is_zero = False
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     visited = []
     cnt = 0
-    for x in range(len(mat)):
-        visited.append([False] * len(mat[0]))
+    for x in range(len(place)):
+        visited.append([False] * len(place[0]))
         
     queue = deque([(a, b, cnt)])
     while queue:
@@ -30,48 +24,36 @@ def bfs(mat, a, b):
         
         for i in range(4):
             nx, ny = x+dx[i], y+dy[i]
-            if nx < 0 or ny < 0 or nx >= len(mat) or ny >= len(mat[0]):
+            if nx < 0 or ny < 0 or nx >= len(place) or ny >= len(place[0]):
                 continue
                 
-            elif visited[nx][ny] or mat[nx][ny] == "X":
-                if cnt > 1:
-                    cnt -= 1
+            elif visited[nx][ny] or place[nx][ny] == "X":
                 continue
                 
-            elif mat[nx][ny] == "P":
+            elif place[nx][ny] == "P":
                 cnt += 1
-
                 visited[nx][ny] = True
-                
+       
                 if (abs(a-nx) + abs(b-ny)) <= 2 and cnt<=2:
                         is_zero = True
                 return is_zero
 
-                
-            elif mat[nx][ny] == "O":
-                cnt += 1
-                queue.append((nx, ny, cnt))
-
+            elif place[nx][ny] == "O":
+                queue.append((nx, ny, cnt+1))
     return False
 
 
 def solution(places):
     answer = []
     for place in places:
-        mat = matrix(place)
-        p_li = loc_p(mat)
+        p_li = loc_p(place)
         is_z = False
         for p in p_li:
-
             l, r = p[0], p[1]
-            if bfs(mat, l, r):
+            if bfs(place, l, r):
                 is_z = True
         if is_z:
             answer.append(0)
         else:
             answer.append(1)
-        
     return answer
-
-# 정확성
-# 96.4 / 100.0
